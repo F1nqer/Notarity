41547a59c3b5ae60a6c1f44baae55dc594b84f1b
a.kulprisov <arthur.kulpeisov@gmail.com>
2022-03-30 22:41:29 +0600

    TinyCleanUp

--- Notarity/Startup.cs
+++ Notarity/Startup.cs
@@ -24,8 +24,7 @@ namespace Notarity
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+=        public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<DocumentService>();
             services.AddControllers();
@@ -35,7 +34,6 @@ namespace Notarity
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
